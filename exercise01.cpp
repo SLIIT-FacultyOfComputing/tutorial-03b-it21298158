11d2b8340f4950844293acc78a738c82b10a9f3d
it21298158 <99331642+it21298158@users.noreply.github.com>
2022-04-29 12:20:13 +0000

    Finish

--- exercise01.cpp
+++ exercise01.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
    float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
    char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
-   cout <<setw(5)<< "No" <<setw(20)<< "Name" << setw(30)<<"Marks" << endl;
+   cout <<setiosflags(ios::fixed)<<setprecision(2)<<setw(5)<< "No" <<setw(20)<< "Name" << setw(30)<<"Marks" << endl;
    for (int r = 0; r < 5; r++) {
        cout <<setw(5)<<  r+1 
             <<setw(20)<<  names[r]
